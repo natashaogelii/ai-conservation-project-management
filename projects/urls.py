c6da2acd3589d93cbc76c5c23f79c7672ea96d6c
Natasha Ogeli <natasha.ogeli@strathmore.edu>
2025-08-29 10:53:06 +0300

    Finished CRUD for projects, ready to start user auth

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ProjectUpdateView
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
     path('add/', views.project_create, name='project_create'),
+    path('<int:pk>/', views.project_detail, name='project-detail'),
+    path('projects/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_update'),
+    path('<int:pk>/delete/', views.project_delete, name='project_delete'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project
 from .forms import ProjectForm 
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
 def project_list(request):
     projects = Project.objects.all()
@@ -17,3 +19,23 @@ def project_create(request):
     return render(request, 'projects/project_form.html', {'form': form})
 
 # Create your views here.
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'projects/project_detail.html', {'project': project})
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = ['name', 'description', 'status']  # include any other fields you want editable
+    template_name = 'projects/project_update.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('project_detail', kwargs={'pk': self.object.pk})
+    
+def project_delete(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+
+    if request.method == "POST":  # confirmation form submitted
+        project.delete()
+        return redirect('project_list')  # back to all projects
+
+    return render(request, 'projects/project_confirm_delete.html', {'project': project})
